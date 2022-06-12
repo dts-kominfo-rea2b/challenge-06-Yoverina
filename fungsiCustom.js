c1288759ed5f19669e70f6fd0c7aa1a6a8cec9ea
Yoverina Nurdin <yoverina_nurdin@yahoo.com>
2022-06-12 19:30:59 +0700

    merge bacaData1,2,3 to bacaData

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -25,59 +25,58 @@ const getSecondWord = (str) => {
 };
 
 const bacaData1 = (data, nextStep) => {
-  fs.readFile(file1,"utf8",(err, dataYangDibaca) => {
-    if(err){
-      return console.log(err);
-    }
-    const arrayOfObject = JSON.parse(dataYangDibaca);
-    data.push(arrayOfObject.message);
-    // data.push(parseData1(dataYangDibaca));
-    nextStep(data); 
-  });
 };
 
 const bacaData2 = (data, nextStep) => {
-  fs.readFile(file2,"utf8",(err, dataYangDibaca) => {
-    if(err){
-      return console.log(err);
-    }
-    const arrayOfObject = JSON.parse(dataYangDibaca);
-    data.push(arrayOfObject[0].message);
-    // data.push(parseData2(dataYangDibaca));
-    nextStep(data); 
-  });
+
 };
 
 const bacaData3 = (data, nextStep) => {
-  fs.readFile(file3,"utf8",(err, dataYangDibaca) => {
-    if(err){
-      return console.log(err);
-    }
-    const arrayOfObject = JSON.parse(dataYangDibaca);
-    data.push(arrayOfObject[0].data.message);
-    const finalData = data.map(getSecondWord); 
-    // console.log("tahap 3: ", data);
-    return console.log(finalData);
-    // console.log("getsecond: ", data.map(getSecondWord));
-  });
+  
 };
 
 function getFinalData(data){
   return data;
 }
 
-const bacaData = () => {
-  const arr = [];
-  bacaData1(arr, (data1) => 
-    bacaData2(data1, (data2) => 
-      bacaData3(data2, (data3) => {
-        // console.log("tahap 3: ", data);
-        // return console.log(data3);
-      })
-    )
-  );
-};
+const bacaData = (callbackfn) => {
+  let data = [];
+  //baca data1
+  fs.readFile(file1,"utf8",(err, dataYangDibaca) => {
+    if(err){
+      callbackfn(err, null);
+    }
+    const arrayOfObject = JSON.parse(dataYangDibaca);
+    data.push(arrayOfObject.message);
+    
+    //baca data2
+    fs.readFile(file2,"utf8",(err, dataYangDibaca) => {
+      if(err){
+        callbackfn(err, null);
+      }
+      const arrayOfObject = JSON.parse(dataYangDibaca);
+      data.push(arrayOfObject[0].message);
 
+      //baca data3
+      fs.readFile(file3,"utf8",(err, dataYangDibaca) => {
+        if(err){
+          callbackfn(err, null);
+        }
+        const arrayOfObject = JSON.parse(dataYangDibaca);
+        data.push(arrayOfObject[0].data.message);
+        callbackfn(null, data.map(getSecondWord));
+      });
+    });
+
+  });
+  // const arr = [];
+  // bacaData1(arr, (data1) => 
+  //   bacaData2(data1, (data2) => 
+  //     bacaData3(data2, (data3) => {
+  //     })
+  //   )
+  // );
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
